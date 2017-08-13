42a72f9c8d3ec0e3548ef630c28fc8f8ed13089b
LauraLaureus <l4depende@gmail.com>
2017-08-13 12:13:37 +0100

    nnpredict

--- nnpredict.m
+++ nnpredict.m
@@ -2,6 +2,7 @@ function [classifyResults] = nnpredict(netModel,data)
 
 classifyResults = [];
 for d=1:size(data,2)
+    %%TODO add data to first param  to netModel and pass it to feedforward
     [~,output] = nnfeedforward(netModel,data(:,2));
     classifyResults(d) = output;
 end
